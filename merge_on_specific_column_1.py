1f7b7ddb63d472c49cb14e50fdf905587efef96c
toliahoang <tung.istu@gmail.com>
2020-05-13 01:41:24 +0700

    Create merge_on_specific_column_1.py

---
+++ merge_on_specific_column_1.py
@@ -0,0 +1,9 @@
+import codecademylib
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+print(orders)
+products = pd.read_csv('products.csv')
+print(products)
+orders_products=pd.merge(orders,products.rename(columns={'id':'customer_id'}))
+print(orders_products)
